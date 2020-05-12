e5f537bd0fe388b9594fba9c701dda57bf0828aa
MyNameIsTrez <welfje@gmail.com>
2020-05-12 21:55:10 +0200

    Wrote basic.py for testing ffmpeg. Can't get 'shortest=1' to work, wrote my first StackOverflow question for it: https://stackoverflow.com/questions/61760400/how-to-use-the-shortest-argument-with-ffmpeg-python

--- code/twitter-bot.js
+++ code/twitter-bot.js
@@ -1,8 +1,8 @@
 const Twit = require("twit");
-const config = require("./config");
+const config = require("../config");
 
 const T = new Twit(config);
-
+console.log(1);
 let IDs = [
   "2405684716", // @welfje
 ];
